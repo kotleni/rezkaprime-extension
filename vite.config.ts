8fc354f9daa4d3d6c273f612f8d6ec66ca494a39
Viktor Varenik <yavarenikya@gmail.com>
2025-07-06 02:46:09 +0300

    chore: try to fix tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,19 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 import {resolve} from 'path';
+import autoprefixer from 'autoprefixer';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
+    css: {
+        postcss: {
+            plugins: [
+                autoprefixer(),
+            ],
+        },
+    },
     build: {
         rollupOptions: {
             input: {
