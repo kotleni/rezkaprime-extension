26ca2495d27c5561c0b9195623a70662714aa5f9
Viktor Varenik <yavarenikya@gmail.com>
2025-07-06 19:17:04 +0300

    feat: remove report button from page

--- src/content/main.tsx
+++ src/content/main.tsx
@@ -2,6 +2,17 @@ import '../index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {ControlPanel} from './toolbar';
+import { doc } from 'prettier';
+
+// TODO: Make similar btn in toolbar
+
+// Remove report button
+const reportBtn = document.getElementById('send-video-issue');
+reportBtn?.remove();
+
+// Remove report button description
+const reportBtnDesc = document.getElementsByClassName('b-post__support_holder');
+reportBtnDesc[0].remove();
 
 const targetElement = document.getElementsByClassName(
     'b-post__social_holder_wrapper',
