8954373842dc39d0cab658e5ba042025f309383e
Viktor Varenik <yavarenikya@gmail.com>
2025-07-06 19:12:41 +0300

    chore: change toolbar destination

--- src/content/main.tsx
+++ src/content/main.tsx
@@ -4,14 +4,8 @@ import ReactDOM from 'react-dom/client';
 import {ControlPanel} from './toolbar';
 
 const targetElement = document.getElementsByClassName(
-    'b-post__rating_table',
+    'b-post__social_holder_wrapper',
 )[0];
-
-// Remove old root if exist
-const oldRoot = targetElement.getElementsByTagName('div');
-if (oldRoot[0]) {
-    oldRoot[0].remove();
-}
 
 if (targetElement) {
     const appRoot = document.createElement('div');
