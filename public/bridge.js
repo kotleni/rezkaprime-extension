d88bd6d4aedd5155ded04ce352427c57b04d4456
Viktor Varenik <yavarenikya@gmail.com>
2025-07-07 19:32:42 +0300

    feat: inject main script without main world

--- public/bridge.js
+++ public/bridge.js
@@ -1,5 +1,17 @@
-/* bridge between page and background */
+function injectScript(filePath) {
+  const script = document.createElement('script');
+  script.setAttribute('type', 'text/javascript');
+  script.setAttribute('src', chrome.runtime.getURL(filePath));
+  (document.head || document.documentElement).appendChild(script);
+  script.onload = () => {
+    script.remove();
+  };
+}
 
+/* inject main script to page world */
+injectScript('content.js');
+
+/* pass events between page and background */
 window.addEventListener("message", (event) => {
   browser.runtime.sendMessage(event.data);
 });
